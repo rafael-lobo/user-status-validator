7ff7f611cdd320aef347bf1aadfb4a7da40c7193
Rafael Lobo <rafaellpalmeira@hotmail.com>
2026-06-28 16:36:35 +0100

    Add User model, migration, and associated tests with factory setup

---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    enum ban_status: {banned: "banned", not_banned: "not_banned"}, default: :not_banned
+
+    validates :idfa, presence: true, uniqueness: true
+    validates :ban_status, presence: true
+end
