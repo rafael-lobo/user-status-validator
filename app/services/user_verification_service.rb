263f39bf00b814b379c8dc83f846601f40d40e8a
Rafael Lobo <rafaellpalmeira@hotmail.com>
2026-07-01 21:00:55 +0100

    Enhance UserVerificationService: ban evaluation logic and VPN detection + caching for VPN checks. (tests GREEN)

--- app/services/user_verification_service.rb
+++ app/services/user_verification_service.rb
@@ -1,39 +1,65 @@
+require "net/http"
+require "json"
+
 class UserVerificationService
-    def initialize(idfa:, rooted_device:, ip:, country_header:)
-      @idfa = idfa
-      @rooted_device = ActiveModel::Type::Boolean.new.cast(rooted_device)
-      @ip = ip
-      @country_header = country_header
+  def initialize(idfa:, rooted_device:, ip:, country_header:)
+    @idfa = idfa
+    @rooted_device = ActiveModel::Type::Boolean.new.cast(rooted_device)
+    @ip = ip
+    @country_header = country_header
+  end
+
+  def call
+    user = User.find_or_initialize_by(idfa: @idfa)
+
+    return user if user.persisted? && user.banned? # Skip checks if already banned
+
+    previous_status = user.ban_status
+    user.ban_status = evaluate_security_status
+
+    if user.save && (user.previously_new_record? || previous_status != user.ban_status) # Log ONLY on creation or status change
+      trigger_log(user.ban_status)
     end
 
-    def call
-      user = User.find_or_initialize_by(idfa: @idfa)
+    user
+  end
 
-      # Rule: Short-circuit if already banned
-      return user if user.persisted? && user.banned?
+  private
 
-      previous_status = user.ban_status
-      user.ban_status = evaluate_security_status
+  def evaluate_security_status
+    return "banned" if @rooted_device == true
+    return "banned" if country_blacklisted?
+    return "banned" if vpn_or_tor_detected?
+    "not_banned"
+  end
 
-      if user.save && (user.previously_new_record? || previous_status != user.ban_status)
-        trigger_log(user.ban_status)
+  def country_blacklisted?
+    return true if @country_header.blank?
+    !Redis.new.sismember("whitelist:countries", @country_header)
+  end
+
+  def vpn_or_tor_detected?
+    # Checks Redis first, if missing -> runs the block and saves for 24h
+    Rails.cache.fetch("vpn_cache:#{@ip}", expires_in: 24.hours) do
+      begin
+        uri = URI("https://vpnapi.io/api/#{@ip}?key=#{ENV['VPNAPI_KEY']}")
+        response = Net::HTTP.get_response(uri)
+
+        # If rate limited/error -> return false to pass the check safely
+        return false unless response.is_a?(Net::HTTPSuccess)
+
+        data = JSON.parse(response.body)
+        data.dig("security", "vpn") || data.dig("security", "tor")
+      rescue StandardError
+        false # Failsafe for network timeouts
       end
-
-      user
     end
+  end
 
-    private
-
-    def evaluate_security_status
-      return "banned" if @rooted_device == true
-      # Add country_blacklisted? and vpn_or_tor_detected? stubs here
-      "not_banned"
-    end
-
-    def trigger_log(status)
-      IntegrityLoggerService.log!({
-        idfa: @idfa, ban_status: status, ip: @ip, rooted_device: @rooted_device,
-        country: @country_header, proxy: false, vpn: false
-      })
-    end
+  def trigger_log(status)
+    IntegrityLoggerService.log!(
+      idfa: @idfa, ban_status: status, ip: @ip, rooted_device: @rooted_device,
+      country: @country_header, proxy: nil, vpn: nil
+    )
+  end
 end
